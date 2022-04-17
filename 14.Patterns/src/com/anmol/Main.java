4813044648331d26bf390a884b516dce2e99a74d
Anmol1804 <anmolgera01@gmail.com>
2022-04-17 11:56:46 +0530

    patterns logic

---
+++ 14.Patterns/src/com/anmol/Main.java
@@ -0,0 +1,126 @@
+package com.anmol;
+
+public class Main {
+    public static void main(String[] args) {
+//        pattern1(4);
+//        pattern2(4);
+//        pattern3(4);
+//        pattern4(5);
+//        pattern5(5);
+//        pattern28(5);
+//        pattern30(5);
+//        pattern17(5);
+        pattern31(4);
+
+    }
+
+    static void pattern1(int n){
+        for (int row = 1; row <=n; row++) {
+            for (int col = 1; col <= n ; col++) {
+                System.out.print("* ");
+            }
+//            when a row printed another row printed in new line
+            System.out.println();
+        }
+    }
+
+    static void pattern2(int n){
+        for (int row = 1; row <=n; row++) {
+            for (int col = 1; col <= row ; col++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+    static void pattern3(int n){
+        for (int row = 1; row <=n; row++) {
+            for (int col = 1; col <= n - row + 1; col++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+    static void pattern4(int n){
+        for (int row = 1; row <=n; row++) {
+            for (int col = 1; col <= row; col++) {
+                System.out.print(col + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    static void pattern5(int n){
+        for (int row = 0; row < 2*n; row++) {
+            int totalColsInrow = (row > n) ? (2*n - row) : row;
+            for (int col = 0; col < totalColsInrow; col++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+    static void pattern28(int n){
+        for (int row = 0; row < 2*n; row++) {
+            int totalColsInrow = (row > n) ? (2*n - row) : row;
+
+            int noOfspaces = n-totalColsInrow;
+            for (int s = 0; s < noOfspaces; s++) {
+                System.out.print(" ");
+            }
+
+            for (int col = 0; col < totalColsInrow; col++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+    static void pattern30(int n){
+        for (int row = 1; row <= n; row++) {
+            int spaces = n-row;
+            for (int s = 1; s <= spaces; s++) {
+                System.out.print("  ");
+            }
+            for (int col = row; col >=1; col--) {
+                System.out.print(col + " ");
+            }
+            for (int col = 2; col <= row; col++) {
+                System.out.print(col + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    static void pattern17(int n){
+        for (int row = 1; row <= 2*n; row++) {
+
+            int c = (row > n) ? (2*n - row) : row;
+
+            int spaces = n-c;
+            for (int s = 1; s <= spaces; s++) {
+                System.out.print("  ");
+            }
+            for (int col = c; col >=1; col--) {
+                System.out.print(col + " ");
+            }
+            for (int col = 2; col <= c; col++) {
+                System.out.print(col + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    static void pattern31(int n){
+        int originalN = n;
+        n = 2*n;
+        for (int row = 0; row <= n; row++) {
+            for (int col = 0; col <=n ; col++) {
+                int atEveryIndex = originalN -  Math.min( Math.min(row, col), Math.min( n-row, n-col));
+                System.out.print(atEveryIndex + " ");
+            }
+            System.out.println();
+        }
+    }
+}
